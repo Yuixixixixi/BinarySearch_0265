8113b71c85a384f75fd66529c013fd667a91656c
yuixixixixi <yendra.paundra@gmail.com>
2025-05-02 09:57:25 +0700

    input arrr

--- file1.cpp
+++ file1.cpp
@@ -3,3 +3,29 @@ using namespace std;
 int element[10];
 int nPanjang;
 int x;
+void input()
+{ // prosedur input
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array: ";
+        cin >> nPanjang;
+        if (nPanjang <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nAraay maksimal 20 elemen.\n";
+        }
+    }
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan elemen array" << endl;
+    cout << "=====================" << endl;
+
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> element[i];
+    }
+}
